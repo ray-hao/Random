59c49dd4bf7bbcc0c6e690d526dbe601bf5cba3b
Ray Hao <71276800+ray-hao@users.noreply.github.com>
2024-08-26 12:37:25 -0400

    Update 2024-08-26.py

--- LeetcodeDaily/2024-08-26.py
+++ LeetcodeDaily/2024-08-26.py
@@ -7,19 +7,53 @@ class Node:
 """
 
 class Solution:
+
+    #
+    # Iterative
+    #
+
     def postorder(self, root: 'Node') -> List[int]:
-        vals = []
-        self.helper(vals, root)
-        return vals
 
+        if not root:
+            return []
+
+        postOrder = []
+        stack = []
+        seen = set()
+        stack.append(root)
+        while len(stack) > 0:
+            popped = stack.pop()
+            unvisitedChild = False
+            for child in popped.children:
+                if child not in seen:
+                    stack.append(popped)
+                    stack.append(child)
+                    unvisitedChild = True
+                    break
+            
+            if unvisitedChild:
+                continue
+            
+            postOrder.append(popped.val)
+            seen.add(popped)
+
+        return postOrder
+    #
     # Recursive
-    def helper(self, vals, currentNode):
-        if not currentNode:
-            return
-        if not currentNode.children:
-            vals.append(currentNode.val)
-            return
+    #
+    
+    # def postorder(self, root: 'Node') -> List[int]:
+    #     vals = []
+    #     self.helper(vals, root)
+    #     return vals
 
-        for child in currentNode.children:
-            self.helper(vals, child)
-        vals.append(currentNode.val)
+    # def helper(self, vals, currentNode):
+    #     if not currentNode:
+    #         return
+    #     if not currentNode.children:
+    #         vals.append(currentNode.val)
+    #         return
+
+    #     for child in currentNode.children:
+    #         self.helper(vals, child)
+    #     vals.append(currentNode.val)
